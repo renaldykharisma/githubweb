c38242d458721b470fe25449b317065aa9068983
renaldykharisma <renaldy@radyalabs.com>
2021-08-18 17:51:32 +0700

    set home component

--- src/routers/router-root.js
+++ src/routers/router-root.js
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import BaseLayout from "../components/base-layout";
+import home from "../components/home";
 
 export default function RouterRoot(){
     return(
         <div>
             <Router>
                 <Switch>
-                    <BaseLayout></BaseLayout>
+                    <BaseLayout>
+                        <Route path="/" component={home}></Route>
+                    </BaseLayout>
                 </Switch>
             </Router>
         </div>
