8397d5a2d1324a3c3f677a6cedaae66585e2b824
renaldykharisma <renaldy@radyalabs.com>
2021-08-19 18:42:34 +0700

    responsive base layout

--- src/components/base-layout.js
+++ src/components/base-layout.js
@@ -1,22 +1,32 @@
 import './base-layout.css';
-import { Layout } from 'antd'
-// import {
-//     GithubOutlined
-//   } from '@ant-design/icons';
+import { Button, Layout } from 'antd';
+import useBreakpoint from 'antd/lib/grid/hooks/useBreakpoint';
+import {
+    GithubFilled
+  } from '@ant-design/icons';
 
 const { Header, Content, Footer } = Layout;
 export default function BaseLayout(props){
+    const screens = useBreakpoint();
     return(
-        <Layout className="layout">
+        <Layout className="layout" breakpoint="xs">
             <Header className="header">
-                <div className="logo"></div>
+                <div className="logo">
+                    <h2 style={{color: 'white'}}>Github Project Web</h2>
+                </div>
+                {
+                    !screens.xs ? 
+                    <div className="githublogo">
+                        <GithubFilled style={{fontSize: 45, color: 'white'}} />&nbsp;&nbsp;
+                        <Button type="link" href={'https://github.com/renaldykharisma/githubweb'}><span style={{fontSize: 20, color: 'white'}}>Source Code</span></Button>
+                    </div> : <></>
+                }
             </Header>
             <Content className="content">
                 <div className="site-layout-content">
                     {props.children}
                 </div>
             </Content>
-            <Footer className="footer">React Github Project Website ©2021 Created by Renaldy Kharisma</Footer>
         </Layout>
     )
 }
