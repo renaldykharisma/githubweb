704685b933873dcc16586359c452c6923c33d6ff
renaldykharisma <renaldy@radyalabs.com>
2021-08-20 15:44:00 +0700

    token git and hide token in repo

--- src/services/base-service.js
+++ src/services/base-service.js
@@ -1,11 +1,12 @@
 import Axios from 'axios';
 import React from 'react';
+import { tokenGit } from '../env';
 
 export default class BaseService extends React.Component{
     async get(urls){
         let result = null;
-
-        await Axios.get(encodeURI(urls))
+        console.log('tokenGit: ', tokenGit);
+        await Axios.get(encodeURI(urls), {headers: {"Authorization": `token ${tokenGit}`}})
         .then(response =>{
             let resp = response;
             result = resp;
